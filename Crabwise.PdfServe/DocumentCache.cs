6e9e467c53c37c2997496f2aaace53fb0beac570
Greg Curtis <gcurtis@crabwisestudios.com>
2011-03-18 15:30:44 -0400

    Stubbed out initialize method for DocumentCache

---
+++ Crabwise.PdfServe/DocumentCache.cs
@@ -0,0 +1,20 @@
+﻿namespace Crabwise.PdfServe.Web.Services
+{
+    using System.IO;
+
+    public static class DocumentCache
+    {
+        public static void Initialize(string documentStoragePath, string templateStoragePath)
+        {
+            if (!Directory.Exists(documentStoragePath))
+            {
+                throw new DirectoryNotFoundException("Could not find the document storage directory.");
+            }
+
+            if (!Directory.Exists(templateStoragePath))
+            {
+                throw new DirectoryNotFoundException("Could not find the template storage directory.");
+            }
+        }
+    }
+}
